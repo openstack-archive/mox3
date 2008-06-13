64b3b1421e561b9c2fdd7e66f8d853889b109489
dglasser <dglasser@b1010a0a-674b-0410-b734-77272b80c875>
2008-06-13 23:00:27 +0000

    Initial import of mox (originally an internal Google project).
    It's not quite ready for release yet, but it's been appropriately
    scrubbed and relicensed.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2.4
+from distutils.core import setup
+# TODO(glasser): Make sure that test stuff is included/documented.
+setup(name='mox',
+      version='1.0.0',
+      py_modules=['mox', 'stubout'],
+      url='http://code.google.com/p/pymox/',
+      maintainer='pymox maintainers',
+      # TODO(glasser): Make pymox-devel list.
+      maintainer_email='pymox-devel@googlegroups.com',
+      license='Apache License, Version 2.0',
+      description='Mock object framework',
+      long_description='''Mox is a mock object framework for Python based on the
+Java mock object framework EasyMock.''',
+      )
