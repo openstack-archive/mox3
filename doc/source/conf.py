c8ca66f6d98540e927951be71c318d6858dceffa
Doug Hellmann <doug@doughellmann.com>
2017-07-06 15:47:45 -0400

    switch from oslosphinx to openstackdocstheme
    Change-Id: I02106accdc6e60911e836b686cf5328537725857
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -8,7 +8,13 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'oslosphinx']
+extensions = ['sphinx.ext.autodoc', 'openstackdocstheme']
+
+# openstackdocstheme options
+repository_name = 'openstack/mox3'
+bug_project = ''
+bug_tag = ''
+html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
 # text edit cycles.
@@ -42,7 +48,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 # html_theme_path = ["."]
-# html_theme = '_theme'
+html_theme = 'openstackdocs'
 html_static_path = ['static']
 
 # Output file base name for HTML help builder.
